3b0b610d2d24a4a9a87cdb30b2dd2ae8d4332142
kamzero <jxr516@163.com>
2021-06-05 21:57:27 +0800

    update index.js with comment schema

--- core/schema/index.js
+++ core/schema/index.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
 const userSchema = require('./userSchema');
 const articleSchema = require('./articleSchema');
+const commentSchema = require('./commentSchema');
 
 exports.userModel = mongoose.model('user', userSchema);
 exports.articleModel = mongoose.model('article', articleSchema);
+exports.commentModel = mongoose.model('comment', commentSchema);
